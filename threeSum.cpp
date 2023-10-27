a3c632df50d6d12abf77ff8872ae29086362f4bd
jarralKeshav <keshavjrall@outlook.com>
2023-10-27 19:23:31 +0530

    27Oct2023

--- threeSum.cpp
+++ threeSum.cpp
@@ -3,35 +3,57 @@ using namespace std;
 
 int main(){
 
-    set<vector<int>> st;
-    vector<int> nums ={-1,0,1};
+    vector<int>nums={-2,0,3,-1,4,0,3,4,1,1,1,-3,-5,4,0};
+    vector<vector<int>>ans;
+    sort(nums.begin(),nums.end());
+
     // int n;
     // cin>>n;
 
-    // for(int i=0;i<n;i++){
+    // for(int inp=0;inp<n;inp++){
     //     int k;
     //     cin>>k;
     //     nums.push_back(k);
     // }
 
-    for(int i=0;i<3;i++){
-        for(int j=i+1;i<3;j++){
-            for(int k=j+1;k<3;k++){
-                if(nums[i]+nums[j]+nums[k]==0){
-                    vector<int>temp ={nums[i],nums[j],nums[k]};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
+    for(int i=0;i<15;i++){
+        if(i!=0 && nums[i]==nums[i-1]) continue;
 
+        int j=i+1;
+        int k=14;
+        while(j<k){
+        int sum = nums[i]+nums[j]+nums[k];
 
-                }
-            }
+        if(sum>0){
+            k--;
+
         }
+        else if(sum<0){
+            j++;
+
+        }
+        else{
+            vector<int> temp = {nums[i],nums[j],nums[k]};
+            ans.push_back(temp);
+
+            j++;
+            k--;
+            while(j<k && nums[j]==nums[j-1]) j++;
+            while(j<k && nums[k]==nums[k+1]) k--;
+
+
+
+        }
+        }
+
+
     }
 
-    vector<vector<int>>ans(st.begin(),st.end());
 
-    for(int i=0;i<ans.size();i++){
-        for(int j=0;j<ans[0].size();j++){
+    int ansSize = ans.size();
+    // int ansColSize = ans[0].size();
+    for(int i=0;i<ansSize;i++){
+        for(int j=0;j<3;j++){
             cout<<ans[i][j]<<" ";
         }
         cout<<"\n";
